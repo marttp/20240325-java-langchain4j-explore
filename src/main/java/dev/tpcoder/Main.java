e9d428a1c25d1d99e13660423862bcf84161156c
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-03-25 23:27:39 +0900

    Enhancement response to stream channel

--- src/main/java/dev/tpcoder/Main.java
+++ src/main/java/dev/tpcoder/Main.java
@@ -1,34 +1,63 @@
 package dev.tpcoder;
 
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.ollama.OllamaChatModel;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.model.StreamingResponseHandler;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
+import dev.langchain4j.model.output.Response;
 
+import java.time.Duration;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 
 public class Main {
     // Ollama serve locally on port 11434
-    private static final String LOCALHOST = "http://localhost:11434";
+    private static final String OLLAMA_HOST = "http://localhost:11434";
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         var model = connectModel("llama2");
         while (true) {
             System.out.print("""
+                    \n
                     Type 'exit' to quit the program.
                     Enter your prompt:\s""");
             String userPrompt = scanner.nextLine();
             if (userPrompt.equals("exit")) {
                 break;
             }
-            String response = model.generate(userPrompt);
-            System.out.printf("Response: %s%n", response);
+            // Change to streaming model
+            modelResponse(model, userPrompt);
         }
     }
 
-    private static ChatLanguageModel connectModel(String modelName) {
-        return OllamaChatModel.builder()
-                .baseUrl(LOCALHOST)
+    private static void modelResponse(StreamingChatLanguageModel model, String userPrompt) {
+        CompletableFuture<Response<AiMessage>> futureResponse = new CompletableFuture<>();
+        model.generate(userPrompt, new StreamingResponseHandler<>() {
+
+            @Override
+            public void onNext(String token) {
+                System.out.print(token);
+            }
+
+            @Override
+            public void onComplete(Response<AiMessage> response) {
+                futureResponse.complete(response);
+            }
+
+            @Override
+            public void onError(Throwable error) {
+                futureResponse.completeExceptionally(error);
+            }
+        });
+        futureResponse.join();
+    }
+
+    private static StreamingChatLanguageModel connectModel(String modelName) {
+        return OllamaStreamingChatModel.builder()
+                .baseUrl(OLLAMA_HOST)
                 .modelName(modelName)
+                .timeout(Duration.ofHours(1))
                 .build();
     }
 }

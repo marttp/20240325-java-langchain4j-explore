44f0afc7f79aba44072692a23e9b7da03ecb4be8
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-03-26 15:57:45 +0900

    Add chat memory with 10 messages

--- src/main/java/dev/tpcoder/ChatService.java
+++ src/main/java/dev/tpcoder/ChatService.java
@@ -1,5 +1,7 @@
 package dev.tpcoder;
 
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.StreamingChatLanguageModel;
 import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
 import dev.langchain4j.service.AiServices;
@@ -15,7 +17,13 @@ public class ChatService implements UserStreamCommunication, ModelCommunication 
 
     public ChatService(String modelUrl, String modelName) {
         this.languageModel = connectModel(modelUrl, modelName);
-        this.assistant = AiServices.create(ModelCommunication.class, this.languageModel);
+        // Memorize for 10 messages continuously
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+        this.assistant = AiServices.builder(ModelCommunication.class)
+                // Alternative of .chatLanguageModel() which support streaming response
+                .streamingChatLanguageModel(this.languageModel)
+                .chatMemory(chatMemory)
+                .build();
     }
 
     // Could you give me the way to exercise for office worker, please?
@@ -24,8 +32,8 @@ public class ChatService implements UserStreamCommunication, ModelCommunication 
         TokenStream tokenStream = chatWithModel(userPrompt);
         CompletableFuture<Void> future = new CompletableFuture<>();
         tokenStream.onNext(System.out::print)
-                .onComplete(token -> {
-                    System.out.println(token);
+                .onComplete(_ -> {
+                    System.out.println();
                     future.complete(null);
                 })
                 .onError(Throwable::printStackTrace)

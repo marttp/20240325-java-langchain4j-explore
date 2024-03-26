c3a89a3ff85baa087d16615f52e63ca8a26b04c3
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-03-26 16:16:35 +0900

    Implement SystemMessage for optimize response

--- src/main/java/dev/tpcoder/ModelCommunication.java
+++ src/main/java/dev/tpcoder/ModelCommunication.java
@@ -1,8 +1,17 @@
 package dev.tpcoder;
 
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.TokenStream;
 
 public interface ModelCommunication {
 
+    @SystemMessage("""
+            You are a professional exercise coaching.
+            You are friendly, polite and concise.
+            And when come to something about the prevention of office syndrome, you are the best adviser.
+            You response will always as bullet points and keep it short in this format
+                * Exercise name - rep - why it needed in short
+            Anyway, The limitation of the exercises are no equipment needed and can be done in a small space e.g. apartment room.
+            """)
     TokenStream chatWithModel(String message);
 }
